22f6323f18a519eef1ff1c05c7b44f0f42b7dbe8
Samik <tosamik@gmail.com>
2021-07-07 23:46:25 +0800

    modified resource name

--- src/main/java/com/sam/ms/userinfo/controllers/AddressController.java
+++ src/main/java/com/sam/ms/userinfo/controllers/AddressController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.sam.ms.userinfo.model.Address;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +24,7 @@ public class AddressController {
         return addressList;
     }
     
-    @GetMapping("v1/address/{id}")
+    @GetMapping("v1/addresses/{id}")
     public Address findAddress(@RequestParam String id) {
         Address address = null;
 
@@ -30,8 +32,13 @@ public class AddressController {
 
     }
 
-    @PostMapping
+    @PostMapping("v1/addresses")
     public void saveAddress(@RequestBody Address address) {
+
+    }
+
+    @PutMapping("v1/addresses/{id}")
+    public void updateAddress(@RequestBody Address address, @PathVariable Long id) {
 
     }
 }
